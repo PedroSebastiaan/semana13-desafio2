7b2457f491b1284cb9bad2bafd81c5c30a5d6291
PedroSebastiaan <pizarropedrov@gmail.com>
2021-08-06 14:50:00 -0400

    add: client functions

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,11 @@
 class Client < ApplicationRecord
     has_many :pets, dependent: :destroy
+
+    def pets_count
+        total_pets = 0
+        self.pets.each do |pet|
+            total_pets += 1
+        end
+        total_pets
+    end
 end
